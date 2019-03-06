ac0e25c62cb26f4ce7664ea60d80277a6b7e5d37
rim <devrealticon.gmail.com>
2019-03-06 17:06:58 +0900

    Initial commit

---
+++ app/src/main/java/com/mrhi2018/ex51fabsnackbar/MainActivity.java
@@ -0,0 +1,32 @@
+package com.mrhi2018.ex51fabsnackbar;
+
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickFab(View view) {
+
+        //토스트랑 비슷한 녀석(다른점 : 버튼같은 액션을 추가할 수 있음)
+        Snackbar snackbar= Snackbar.make(view, "click!!", Snackbar.LENGTH_INDEFINITE);
+        snackbar.setAction("Submit", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "click submit", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        snackbar.show();
+
+
+    }
+}
